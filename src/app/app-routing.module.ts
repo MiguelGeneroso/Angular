b24ff27f9ea0983680094b63687b7488508ee66b
MiguelGeneroso <gene17051996@gmail.com>
2022-11-29 10:55:03 +0100

    sunida dia 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {RegistroComponent} from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component : HomeComponent},
+  {path: "registro", component : RegistroComponent},
+  {path: '',   redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
